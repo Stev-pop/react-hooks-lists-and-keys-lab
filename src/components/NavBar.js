ef73ce8aec03cd1014fadc708f4fd21667d4ee88
Stephen Onyango <onyangosteve01@gmail.com>
2024-05-03 22:36:14 +0300

    Adds assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksList = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+
+return(
+  <nav>
+    return(
+      {linksList}
+    )
+  </nav>
+
+)
 }
 
 export default NavBar;
